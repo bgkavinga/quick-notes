65f77f7d393b8b3779ded78f4f7291e7e03a10b6
kavingas <kavingas@adobe.com>
2024-08-23 10:50:16 +1000

    context menu and search improvments

--- src/components/NoteComponent.tsx
+++ src/components/NoteComponent.tsx
@@ -2,7 +2,6 @@ import React from 'react'
 import { useNavigate } from 'react-router-dom'
 import { FaTrash, FaEdit, FaCopy, FaEye, FaTimes } from 'react-icons/fa'
 import MarkdownRenderer from '@/components/MarkdownRenderer'
-import truncate from '@/utils/truncate'
 import { Note, useNoteContext } from '@/context/NoteContext'
 import useSearchManager from '@/hooks/useSearchManager'
 import useNotificationManager from '@/hooks/useNotificationManager'
@@ -28,11 +27,16 @@ const NoteComponent: React.FC<Note> = note => {
   return (
     <>
       <div className='relative group'>
-        <div className='flex items-center bg-slate-200 text-gray-800 justify-between'>
-          <h1 className='text-sm font-bold p-2 cursor-pointer flex-grow' onClick={() => navigate(`/note-detail/${note.id}`)}>
-            {note.title ? truncate(note.title || '', 55) : 'Untitled'}
-          </h1>
-          <div className='flex space-x-4 p-2 ml-2'>
+        <div className='flex w-full items-center bg-slate-200 text-gray-800 justify-between'>
+          <div className='w-4/5'>
+            <h1
+              className='text-sm font-bold p-2 cursor-pointer overflow-hidden text-ellipsis whitespace-nowrap'
+              onClick={() => navigate(`/note-detail/${note.id}`)}
+            >
+              {note.title ? note.title : 'Untitled'}
+            </h1>
+          </div>
+          <div className='flex space-x-4 p-2 mr-2 w-1/5'>
             <FaEdit
               className='cursor-pointer text-yellow-400 hover:text-yellow-600 transition-colors duration-300 text-lg'
               onClick={() => navigate(`/note-update/${note.id}`)}
@@ -57,26 +61,30 @@ const NoteComponent: React.FC<Note> = note => {
       <div className='tags-list flex justify-between items-center mt-2'>
         <ul className='flex flex-wrap p-2 space-x-2'>
           {note.tags &&
-            note.tags.map(tag => (
-              tag!='context' && <li
-                key={tag}
-                className={`text-sm tag-item px-3 py-1 mt-1 cursor-pointer transition-colors duration-300 ${
-                  selectedTags.includes(tag)
-                    ? generateTagStyles({ name: tag }, true)
-                    : generateTagStyles({ name: tag }, false)
-                }`}
-              >
-                <span className='inline-flex items-center text-sm font-medium'
-                key={tag}
-                >
-                <a onClick={() => handleTagClick(tag)}>  {tag} </a> 
-                <FaTimes
-                    className='ml-2 cursor-pointer text-red-500 bg-gray-200 hover:text-red-700 transition-colors duration-300'
-                    onClick={() => handleRemoveTag(tag)}
-                  />
-                </span>
-              </li>
-            ))}
+            note.tags.map(
+              tag =>
+                tag != 'context' && (
+                  <li
+                    key={tag}
+                    className={`text-sm tag-item px-3 py-1 mt-1 cursor-pointer transition-colors duration-300 ${
+                      selectedTags.includes(tag)
+                        ? generateTagStyles({ name: tag }, true)
+                        : generateTagStyles({ name: tag }, false)
+                    }`}
+                  >
+                    <span
+                      className='inline-flex items-center text-sm font-medium'
+                      key={tag}
+                    >
+                      <a onClick={() => handleTagClick(tag)}> {tag} </a>
+                      <FaTimes
+                        className='ml-2 cursor-pointer text-red-500 bg-gray-200 hover:text-red-700 transition-colors duration-300'
+                        onClick={() => handleRemoveTag(tag)}
+                      />
+                    </span>
+                  </li>
+                )
+            )}
         </ul>
       </div>
     </>
